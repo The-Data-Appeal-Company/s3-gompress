0473de7c0828e95ee7eabb33899895da963a89ac
Lorenzo Torracchi <lorenzotorracchi@mail.com>
2020-07-22 17:14:48 +0200

    reanabled s3 test with localstack

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # s3-gompress
+
+![Go](https://github.com/The-Data-Appeal-Company/s3-gompress/workflows/Go/badge.svg)
+![Go](https://github.com/The-Data-Appeal-Company/s3-gompress/workflows/Go/badge.svg?branch=master)
+[![license](https://img.shields.io/github/license/The-Data-Appeal-Company/s3-gompress.svg)](LICENSE)
+
+### Simple AWS S3 client with compression/decompression for files
+S3-gompressor allows getting and putting objects on S3 without worrying about decompression/compression
+
+### Usage 
+
+```go
+    s3Client := NewS3CompressorClient()
+``` 
--- client/s3_plain_client_test.go
+++ client/s3_plain_client_test.go
@@ -4,37 +4,28 @@ import (
 	"fmt"
 	"github.com/The-Data-Appeal-Company/s3-gompress/test"
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/client"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/mitchelldavis/go_localstack/pkg/localstack"
 	log "github.com/sirupsen/logrus"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 	"io/ioutil"
-	"reflect"
 	"testing"
 )
 
 var LOCALSTACK *localstack.Localstack
 var bucketName = "bachettaccio"
 
-type SetupLocalStack struct {
+type LocalStackTestSuite struct {
+	suite.Suite
 }
 
-func (s SetupLocalStack) BeforeTest(suiteName, testName string) {
-	InitializeLocalstack()
+func TestS3PlainClientSuite(t *testing.T) {
+	InitializeLocalStack()
+	defer LOCALSTACK.Destroy()
+	suite.Run(t, new(LocalStackTestSuite))
 }
 
-type TearDownLocalStack struct {
-}
-
-func (t TearDownLocalStack) AfterTest(suiteName, testName string) {
-	err := LOCALSTACK.Destroy()
-	if err != nil {
-		log.Error(err)
-	}
-}
-
-func InitializeLocalstack() {
+func InitializeLocalStack() {
 	s3, _ := localstack.NewLocalstackService("s3")
 
 	// Gather them all up...
@@ -61,156 +52,109 @@ func initStack(svc *s3.S3) error {
 	return err
 }
 
-func S3PlainClient_Get(t *testing.T) {
+func (suite *LocalStackTestSuite) TestShouldGetFromS3() {
 	sess := LOCALSTACK.CreateAWSSession()
 
 	svc := s3.New(sess)
 	err := initStack(svc)
-
 	if err != nil {
-		t.Fatal(err)
+		suite.Fail(err.Error())
 	}
-	type fields struct {
-		client client.ConfigProvider
-		bucket string
+	s := NewS3PlainClient(
+		sess,
+		bucketName,
+	)
+	want := test.ReadFileOrError(suite.T(), "test_data/input.json")
+	_ = s.Put("input.json", want)
+	got, err := s.Get("input.json")
+	if err != nil {
+		suite.Fail(err.Error())
 	}
-	type args struct {
-		key string
-	}
-	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		want    []byte
-		wantErr bool
-	}{
-		{
-			name: "shouldGetFromS3",
-			fields: fields{
-				client: sess,
-				bucket: bucketName,
-			},
-			args: args{
-				key: "input.json",
-			},
-			want:    test.ReadFileOrError(t, "test_data/input.json"),
-			wantErr: false,
-		},
-		{
-			name: "shouldErrorIfBucketNotExists",
-			fields: fields{
-				client: sess,
-				bucket: "non-esisto",
-			},
-			args: args{
-				key: "input.json",
-			},
-			want:    []byte{},
-			wantErr: true,
-		},
-		{
-			name: "shouldErrorIfFileNotExists",
-			fields: fields{
-				client: sess,
-				bucket: "non-esisto",
-			},
-			args: args{
-				key: "nullaaa.json",
-			},
-			want:    []byte{},
-			wantErr: true,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			s := NewS3PlainClient(
-				tt.fields.client,
-				tt.fields.bucket,
-			)
-			if tt.args.key == "input.json" {
-				_ = s.Put("input.json", test.ReadFileOrError(t, "test_data/input.json"))
-			}
-			got, err := s.Get(tt.args.key)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("Get() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Get() got = %v, want %v", got, tt.want)
-			}
-		})
-	}
+	suite.Equal(got, want)
 }
 
-func S3PlainClient_Put(t *testing.T) {
+func (suite *LocalStackTestSuite) TestShouldErrorIfBucketNotExistsGet() {
+	sess := LOCALSTACK.CreateAWSSession()
+
+	svc := s3.New(sess)
+	err := initStack(svc)
+	if err != nil {
+		suite.Fail(err.Error())
+	}
+	s := NewS3PlainClient(
+		sess,
+		"non-esisto",
+	)
+
+	_, err = s.Get("input.json")
+	suite.Error(err)
+}
+
+func (suite *LocalStackTestSuite) TestShouldErrorIfFileNotExists() {
+	sess := LOCALSTACK.CreateAWSSession()
+
+	svc := s3.New(sess)
+	err := initStack(svc)
+	if err != nil {
+		suite.Fail(err.Error())
+	}
+	s := NewS3PlainClient(
+		sess,
+		"non-esisto",
+	)
+
+	_ = s.Put("input.json", test.ReadFileOrError(suite.T(), "test_data/input.json"))
+	_, err = s.Get("nullaaa.json")
+	suite.Error(err)
+}
+
+func (suite *LocalStackTestSuite) TestShouldPutOnS3() {
 	sess := LOCALSTACK.CreateAWSSession()
 
 	svc := s3.New(sess)
 	err := initStack(svc)
 
 	if err != nil {
-		t.Fatal(err)
+		suite.Fail(err.Error())
 	}
-	type fields struct {
-		client client.ConfigProvider
-		bucket string
-	}
-	type args struct {
-		key    string
-		object []byte
-	}
-	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		wantErr bool
-	}{
-		{
-			name: "shouldPutOnS3",
-			fields: fields{
-				client: sess,
-				bucket: bucketName,
-			},
-			args: args{
-				key:    "antani.json",
-				object: test.ReadFileOrError(t, "test_data/input.json"),
-			},
-			wantErr: false,
-		},
-		{
-			name: "shouldErrorIfBucketNotExists",
-			fields: fields{
-				client: sess,
-				bucket: "non-esisto",
-			},
-			args: args{
-				key: "antani.json",
-			},
-			wantErr: true,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			s := NewS3PlainClient(
-				tt.fields.client,
-				tt.fields.bucket,
-			)
-			if err := s.Put(tt.args.key, tt.args.object); (err != nil) != tt.wantErr {
-				t.Errorf("Put() error = %v, wantErr %v", err, tt.wantErr)
-			}
-			if !tt.wantErr {
-				object, err := svc.GetObject(&s3.GetObjectInput{
-					Bucket: aws.String(tt.fields.bucket),
-					Key:    aws.String(tt.args.key),
-				})
+	s := NewS3PlainClient(
+		sess,
+		bucketName,
+	)
+	key := "antani.json"
+	obj := test.ReadFileOrError(suite.T(), "test_data/input.json")
+	err = s.Put(key, obj)
+	suite.NoError(err)
 
-				if err != nil {
-					t.Fatal(err)
-				}
-				all, err := ioutil.ReadAll(object.Body)
-				assert.NoError(t, err)
-				assert.Equal(t, tt.args.object, all)
-			}
-		})
+	object, err := svc.GetObject(&s3.GetObjectInput{
+		Bucket: aws.String(bucketName),
+		Key:    aws.String(key),
+	})
+
+	if err != nil {
+		suite.Fail(err.Error())
 	}
+	suite.NotNil(object)
+	all, err := ioutil.ReadAll(object.Body)
+	suite.NoError(err)
+	suite.Equal(obj, all)
+}
+
+func (suite *LocalStackTestSuite) TestShouldErrorIfBucketNotExistsPut() {
+	sess := LOCALSTACK.CreateAWSSession()
+
+	svc := s3.New(sess)
+	err := initStack(svc)
+
+	if err != nil {
+		suite.Fail(err.Error())
+	}
+	s := NewS3PlainClient(
+		sess,
+		"non-esisto",
+	)
+	key := "antani.json"
+	obj := test.ReadFileOrError(suite.T(), "test_data/input.json")
+	err = s.Put(key, obj)
+	suite.Error(err)
 }
